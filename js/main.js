d142ce38f26ab08474b864e1c23fd10b76d17ed0
kutuz00 <kutuz00@gmail.com>
2022-01-31 21:52:00 -0800

    Date calculate from webinar

--- js/main.js
+++ js/main.js
@@ -1,10 +1,33 @@
 import { showDateCalc, showTimer } from "./tabs.js";
 import "./../node_modules/howler/dist/howler.core.min.js";
+import { diffDates, diffToHtml } from "./datecalc.js"; // 1
+import { formatError } from "./utils.js"; // 2
+
+const dateCalcForm = document.getElementById("datecalc");
+const dateCalcResult = document.getElementById("datecalc__result");
+
 const tabs = document.querySelector(".tab").getElementsByTagName("button");
 const timer = document.querySelector(".timer-display");
 const form = document.getElementById("timer");
 let countDownId;
-//console.log();
+
+dateCalcForm.addEventListener("submit", handleCalcDates);
+
+function handleCalcDates(event) {
+  dateCalcResult.innerHTML = "";
+  event.preventDefault();
+
+  let { firstDate, secondDate } = event.target.elements;
+  (firstDate = firstDate.value), (secondDate = secondDate.value);
+
+  if (firstDate && secondDate) {
+    const diff = diffDates(firstDate, secondDate); // 3
+    dateCalcResult.innerHTML = diffToHtml(diff); // 4
+  } else
+    dateCalcResult.innerHTML = formatError(
+      "Для расчета промежутка необходимо заполнить оба поля"
+    ); // 5
+}
 for (let i = 0; i < tabs.length; i++) {
   tabs[i].onclick = (e) => {
     e.preventDefault();
